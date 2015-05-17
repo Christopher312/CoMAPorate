f9333aa5ded46c99ad95940fdc202d52e6ff6118
SteeredPrune98 <maheswarimurali@yahoo.com>
2015-05-16 23:25:26 -0400

    bootstrap

--- respondToClick.js
+++ respondToClick.js
@@ -14,9 +14,8 @@ function show_image(src, width, height, alt, x, y) {
     img.style.position="absolute";
     img.style.top= y-60;
     img.style.left= x-15;
-    console.log(x,y)
     img.id="waypoint";
-    document.getElementById("imageContainer").appendChild(img);
+    document.getElementById("imageContainer").appendChild(img); 
 }
 $("#waypoint").click(
     function(e){
